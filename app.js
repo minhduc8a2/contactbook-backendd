f1c95f42c12a6b2d8b92e3f8083ee30d039f09c1
minhduc <minhduc8a2.1@gmail.com>
2024-02-23 15:24:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/contacts", contactsRouter);   
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+    
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to contact book application." })
 })
